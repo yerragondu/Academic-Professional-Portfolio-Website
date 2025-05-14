2d7205b9a0a988c1e1eb381cbdcfa83da8195b28
NIKHIL REDDY YERRAGONDU <92078883+yerragondu@users.noreply.github.com>
2025-05-14 18:21:25 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1-Bf1vIDokdAlCPFzeA6WmRGsZvLwxZ7l/view?usp=sharing" target="_blank">Resume</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1hAXxAMTokJ1umRNuAXhXAdy7SI-a0Cjx/view?usp=sharing" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
